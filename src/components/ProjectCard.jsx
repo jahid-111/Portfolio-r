22dd0cd31c3730d6a76d33f09118d069e3cd6418
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-08-29 04:52:12 +0300

    load_project_data

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,9 +2,8 @@ import { Link } from "react-router-dom";
 import SearchInput from "./SearchInput";
 import PageIntro from "./PageIntro";
 import data from "../data";
+import Reval from "./animation/Reval";
 
-const image =
-  "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png";
 const ProjectCard = () => {
   // console.log(data);
   return (
@@ -14,116 +13,84 @@ const ProjectCard = () => {
       <div className=" my-14  md:px-0">
         <SearchInput />
       </div>
+
       <h5 className=" text-2xl my-4">Total Project : {data.projects.length}</h5>
+
       <div className="grid sm:grid-cols-2 gap-7 mx-auto justify-center items-center">
         {data?.projects?.map((project, i) => (
-          <div
-            key={i}
-            className="max-w-full  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-          >
-            <Link to="#">
-              <img className="rounded-t-lg  w-full h-full" src={image} alt="" />
-            </Link>
+          <Reval key={i}>
+            <div className="max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <Link to="#">
+                <img
+                  className="rounded-t-lg w-full  h-64 md:h-96"
+                  src={project.thumbnail}
+                  alt=""
+                />
+              </Link>
 
-            <div className=" p-2">
-              <div className=" my-2 flex justify-between items-baseline border-b pb-4">
-                <div>
-                  <Link to="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                      {project?.title}
-                    </h5>
-                  </Link>
+              <div className="p-2">
+                <div className="my-2 flex justify-between items-baseline border-b pb-4">
+                  <div>
+                    <Link to="#">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        {project?.title}
+                      </h5>
+                    </Link>
 
-                  <div className="  w-full ">
-                    {project?.technologies?.map((technology, index) => (
-                      <div key={index} className="mt-3 me-2 inline-block ">
-                        <Link
-                          key={index}
-                          to="#"
-                          className="text-wrap rounded-md button-primary p-1 sm:-2 "
+                    <div className="w-full">
+                      {project?.technologies?.map((technology, index) => (
+                        <div key={index} className="mt-3  me-2 inline-block">
+                          <Link
+                            to="#"
+                            className="text-wrap rounded-md button-primary py-1 px-2 sm:-2"
+                          >
+                            {technology}
+                          </Link>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                  <div>
+                    <a
+                      href={project.liveLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <svg
+                        className="h-8 w-8"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 256 256"
+                        xmlSpace="preserve"
+                      >
+                        <defs></defs>
+                        <g
+                          style={{
+                            stroke: "none",
+                            fill: "rgb(0,0,0)",
+                            opacity: 1,
+                          }}
+                          transform="translate(1.4 1.4) scale(2.81 2.81)"
                         >
-                          {technology}
-                        </Link>
-                      </div>
-                    ))}
+                          <path
+                            d="M 88.411 90 H 1.59 C 0.712 90 0 89.288 0 88.41 V 1.59 C 0 0.712 0.712 0 1.59 0 h 34.831 c 0.848 0 1.536 0.688 1.536 1.536 V 15.43 c 0 0.848 -0.688 1.536 -1.536 1.536 H 16.967 v 56.066 h 56.066 V 51.932 c 0 -0.848 0.688 -1.536 1.536 -1.536 h 13.894 c 0.848 0 1.536 0.688 1.536 1.536 v 36.478 C 90 89.288 89.288 90 88.411 90 z"
+                            strokeLinecap="round"
+                          />
+                          <path
+                            d="M 88.943 38.494 H 73.794 c -0.584 0 -1.057 -0.473 -1.057 -1.057 v -7.968 L 53.27 48.936 c -0.442 0.442 -1.157 0.442 -1.599 0 L 41.064 38.329 c -0.442 -0.442 -0.442 -1.157 0 -1.599 l 19.467 -19.467 h -7.968 c -0.584 0 -1.057 -0.473 -1.057 -1.057 V 1.057 C 51.506 0.473 51.979 0 52.563 0 h 21.231 h 15.149 C 89.527 0 90 0.473 90 1.057 v 15.149 v 21.231 C 90 38.021 89.527 38.494 88.943 38.494 z"
+                            strokeLinecap="round"
+                          />
+                        </g>
+                      </svg>
+                    </a>
                   </div>
                 </div>
-                <div>
-                  {" "}
-                  <a
-                    href={project.liveLink}
-                    className=""
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <svg
-                      className=" h-8 w-8"
-                      xmlns="http://www.w3.org/2000/svg"
-                      xmlnsXlink="http://www.w3.org/1999/xlink"
-                      version="1.1"
-                      width="256"
-                      height="256"
-                      viewBox="0 0 256 256"
-                      xmlSpace="preserve"
-                    >
-                      <defs></defs>
-                      <g
-                        style={{
-                          stroke: "none",
-                          strokeWidth: 0,
-                          strokeDasharray: "none",
-                          strokeLinecap: "butt",
-                          strokeLinejoin: "miter",
-                          strokeMiterlimit: 10,
-                          fill: "none",
-                          fillRule: "nonzero",
-                          opacity: 1,
-                        }}
-                        transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)"
-                      >
-                        <path
-                          d="M 88.411 90 H 1.59 C 0.712 90 0 89.288 0 88.41 V 1.59 C 0 0.712 0.712 0 1.59 0 h 34.831 c 0.848 0 1.536 0.688 1.536 1.536 V 15.43 c 0 0.848 -0.688 1.536 -1.536 1.536 H 16.967 v 56.066 h 56.066 V 51.932 c 0 -0.848 0.688 -1.536 1.536 -1.536 h 13.894 c 0.848 0 1.536 0.688 1.536 1.536 v 36.478 C 90 89.288 89.288 90 88.411 90 z"
-                          style={{
-                            stroke: "none",
-                            strokeWidth: 1,
-                            strokeDasharray: "none",
-                            strokeLinecap: "butt",
-                            strokeLinejoin: "miter",
-                            strokeMiterlimit: 10,
-                            fill: "rgb(0,0,0)",
-                            fillRule: "nonzero",
-                            opacity: 1,
-                          }}
-                          transform="matrix(1 0 0 1 0 0)"
-                          strokeLinecap="round"
-                        />
-                        <path
-                          d="M 88.943 38.494 H 73.794 c -0.584 0 -1.057 -0.473 -1.057 -1.057 v -7.968 L 53.27 48.936 c -0.442 0.442 -1.157 0.442 -1.599 0 L 41.064 38.329 c -0.442 -0.442 -0.442 -1.157 0 -1.599 l 19.467 -19.467 h -7.968 c -0.584 0 -1.057 -0.473 -1.057 -1.057 V 1.057 C 51.506 0.473 51.979 0 52.563 0 h 21.231 h 15.149 C 89.527 0 90 0.473 90 1.057 v 15.149 v 21.231 C 90 38.021 89.527 38.494 88.943 38.494 z"
-                          style={{
-                            stroke: "none",
-                            strokeWidth: 1,
-                            strokeDasharray: "none",
-                            strokeLinecap: "butt",
-                            strokeLinejoin: "miter",
-                            strokeMiterlimit: 10,
-                            fill: "rgb(0,0,0)",
-                            fillRule: "nonzero",
-                            opacity: 1,
-                          }}
-                          transform="matrix(1 0 0 1 0 0)"
-                          strokeLinecap="round"
-                        />
-                      </g>
-                    </svg>
-                  </a>
-                </div>
-              </div>
 
-              <p className=" font-normal text-gray-700 dark:text-gray-400">
-                {project?.description}
-              </p>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                  {project?.description}
+                </p>
+              </div>
             </div>
-          </div>
+          </Reval>
         ))}
       </div>
     </div>
--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,20 @@
+import weather from "../assets/image/weather_image.png";
+import astroShipThumbnail from "../assets/image/astro_web.jpg";
+import music from "../assets/image/music.jpg";
+import foodKitchen from "../assets/image/food_kitchen.jpg";
+import phoneSearch from "../assets/image/phone_search.jpg";
+import letsJucyCoctail from "../assets/image/lets_Jucy.jpg";
+import smartGadget from "../assets/image/smart.gadget.jpg";
+import quizImage from "../assets/image/quiz_image.jpg";
+import gym from "../assets/image/gym.jpg";
+import ticTacToe from "../assets/image/tic_tac_toe.png";
+import learnerImage from "../assets/image/learner_image.png";
+import taskList from "../assets/image/task_list.png";
+import Bookshelf from "../assets/image/book.jpg";
+import movie from "../assets/image/movie.jpg";
+import news from "../assets/image/news.jpg";
+import fakebook from "../assets/image/fakebookjpg.jpg";
+import blogify from "../assets/image/blogify.png";
 
 const data = {
   home: [
@@ -23,154 +40,137 @@ const data = {
     {
       title: "Weather",
       description:
-        "A React component for displaying and managing blog comments, including author information and profile images fetched from a server.",
-      technologies: ["Vanilla JS", "Tailwind Css"],
-      thumbnail:
-        "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png",
+        "A simple weather application built with Vanilla JavaScript and styled using Tailwind CSS. It fetches real-time weather data from an API and displays the current conditions, including temperature, and humidity for any location.",
+      technologies: ["Vanilla JS", "Tailwind Css", "API"],
+      thumbnail: weather,
       liveLink: "https://weather-wheat-xi-28.vercel.app/",
     },
     {
       title: "Astro landing page",
       description:
-        "A React component for displaying and managing blog comments, including author information and profile images fetched from a server.",
+        "AstroShip is a visually stunning, responsive website that offers a captivating user experience with space-themed designs. Built with modern web technologies, it showcases interactive elements and smooth animations, making it an ideal platform for exploring space-related content.",
       technologies: ["Vanilla JS", "Tailwind Css"],
-      thumbnail:
-        "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png",
+      thumbnail: astroShipThumbnail,
       liveLink: "https://astroship-website-chi.vercel.app/",
     },
     {
       title: "Play Music",
       description:
-        "A React component for displaying and managing blog comments, including author information and profile images fetched from a server.",
+        "An interactive music player where pressing different keys on the keyboard triggers various sounds. Built with Vanilla JavaScript and styled using Tailwind CSS, this project creates a fun and engaging way to play musical notes or beats directly from the keyboard.",
       technologies: ["Vanilla JS", "Tailwind Css"],
-      thumbnail:
-        "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png",
+      thumbnail: music,
       liveLink: "https://drum-play.vercel.app/",
     },
     {
       title: "Food Kitchen",
       description:
-        "A React component for displaying and managing blog comments, including author information and profile images fetched from a server.",
+        "A web application that helps users discover and explore various recipes. Built with Vanilla JavaScript and styled using BootStrap CSS, it features a user-friendly interface where users can search for recipes by ingredients, cuisine, or meal type, with detailed instructions and images.",
       technologies: ["Vanilla JS", "BootStrap CSS"],
-      thumbnail:
-        "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png",
+      thumbnail: foodKitchen,
       liveLink: "https://meal-db-one.vercel.app/",
     },
     {
       title: "Phone Search",
       description:
-        "A React component for displaying and managing blog comments, including author information and profile images fetched from a server.",
-      technologies: ["Vanilla JS", "BootStrap CSS"],
-      thumbnail:
-        "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png",
+        "A web application designed to help users search for various phones and their specifications. Built with Vanilla JavaScript and styled using BootStrap CSS, this tool allows users to find detailed information about different phone models, including features, prices, and reviews.",
+      technologies: ["Vanilla JS", "BootStrap CSS", "Custom api"],
+      thumbnail: phoneSearch,
       liveLink: "https://phone-market-ten.vercel.app/",
     },
     {
-      title: "Let's Jucy",
+      title: "Jucy Coctail",
       description:
-        "A React component for displaying and managing blog comments, including author information and profile images fetched from a server.",
-      technologies: ["Vanilla JS", "BootStrap CSS"],
-      thumbnail:
-        "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png",
+        "A responsive website showcasing a variety of cocktail recipes. Built with HTML and styled using BootStrap CSS, it provides an engaging user experience with a mobile-friendly design and interactive features for exploring different cocktail options.",
+      technologies: ["HTML", "BootStrap CSS"],
+      thumbnail: letsJucyCoctail,
       liveLink: "https://coctail-tau.vercel.app/#",
     },
     {
       title: "Smart Gadget",
       description:
-        "A React component for displaying and managing blog comments, including author information and profile images fetched from a server.",
+        "An interactive functional web application designed to showcase and manage various smart gadgets and tech products. Built with Vanilla JavaScript and styled using BootStrap CSS, this site offers a user-friendly interface to explore product details, compare features, and stay updated with the latest tech trends.",
       technologies: ["Vanilla JS", "BootStrap CSS"],
-      thumbnail:
-        "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png",
+      thumbnail: smartGadget,
       liveLink: "https://smart-gadget-coral.vercel.app/",
     },
     {
       title: "Quiz For Dev",
       description:
-        "A React component for displaying and managing blog comments, including author information and profile images fetched from a server.",
-      technologies: ["Vanilla JS", "BootStrap CSS"],
-      thumbnail:
-        "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png",
+        "An interactive quiz application designed for developers to test their knowledge on various programming topics are React, Javascript, CSS, Git. Built with ReactJS and styled using BootStrap CSS. making it an engaging tool for learning and self-assessment.",
+      technologies: ["ReactJs", "BootStrap CSS"],
+      thumbnail: quizImage,
       liveLink: "https://dev-quiz-ashy.vercel.app/",
     },
     {
       title: "Strong Gym",
       description:
-        "A React component for displaying and managing blog comments, including author information and profile images fetched from a server.",
-      technologies: ["Vanilla JS", "Tailwind CSS"],
-      thumbnail:
-        "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png",
+        "A beautifully designed web application showcasing a gym's services and facilities. Created with React.js and styled using Tailwind CSS, this project highlights modern UI/UX design principles, featuring a visually appealing layout that enhances user engagement and showcases fitness offerings effectively.",
+      technologies: ["ReactJs", "Tailwind CSS"],
+      thumbnail: gym,
       liveLink: "https://gym-nine-indol.vercel.app/",
     },
     {
       title: "Tic-tac-toe",
       description:
-        "A React component for displaying and managing blog comments, including author information and profile images fetched from a server.",
-      technologies: ["Vanilla JS", "Tailwind Css"],
-      thumbnail:
-        "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png",
+        "A classic Tic-tac-toe game built with Vanilla JavaScript and styled using Tailwind CSS. This interactive game allows users to play against each other or the computer, with a clean and responsive design that adapts to different screen sizes.",
+      technologies: ["ReactJs", "Tailwind Css"],
+      thumbnail: ticTacToe,
       liveLink: "https://tic-tac-toe-bice-mu.vercel.app/",
     },
     {
       title: "Learner Grade",
       description:
-        "A React component for displaying and managing blog comments, including author information and profile images fetched from a server.",
-      technologies: ["Vanilla JS", "Tailwind Css"],
-      thumbnail:
-        "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png",
+        "A visually appealing design concept for a learner grades management system. Created with React.js and styled using Tailwind CSS, this project showcases a clean and modern interface for displaying grades and performance metrics, focusing on aesthetic presentation rather than functionality.",
+      technologies: ["React.js", "Tailwind CSS"],
+      thumbnail: learnerImage,
       liveLink: "https://learners-list.vercel.app/",
     },
     {
       title: "Task List",
       description:
-        "A React component for displaying and managing blog comments, including author information and profile images fetched from a server.",
-      technologies: ["Vanilla JS", "Tailwind Css"],
-      thumbnail:
-        "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png",
+        "A React.js application for task management, enhanced with Tailwind CSS styling. Provides a user-friendly interface for adding, viewing, and organizing tasks. Includes features for marking tasks as completed and tracking progress. Ideal for efficient task management and productivity.",
+      technologies: ["React.js", "Tailwind CSS"],
+      thumbnail: taskList,
       liveLink: "https://task-managers-pink.vercel.app/",
     },
     {
       title: "Bookshelf",
       description:
-        "A React component for displaying and managing blog comments, including author information and profile images fetched from a server.",
-      technologies: ["Vanilla JS", "Tailwind Css"],
-      thumbnail:
-        "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png",
+        "A React.js application for managing and displaying your book collection, styled with Tailwind CSS. Features a clean interface for adding, organizing, and viewing books. Ideal for book enthusiasts to track and showcase their library.",
+      technologies: ["React.js", "Tailwind CSS"],
+      thumbnail: Bookshelf,
       liveLink: "https://book-finder-react-brown.vercel.app/",
     },
     {
       title: "Movie Cine",
       description:
-        "A React component for displaying and managing blog comments, including author information and profile images fetched from a server.",
-      technologies: ["Vanilla JS", "Tailwind Css"],
-      thumbnail:
-        "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png",
+        "A React.js application for browsing and managing movie information, styled with Tailwind CSS. Offers a sleek interface for searching, viewing, and organizing movies. Perfect for movie enthusiasts to explore and manage their favorite films.",
+      technologies: ["React.js", "Tailwind CSS"],
+      thumbnail: movie,
       liveLink: "https://movie-cine-iota.vercel.app/",
     },
     {
       title: "NEWS Feed",
       description:
-        "A React component for displaying and managing blog comments, including author information and profile images fetched from a server.",
-      technologies: ["Vanilla JS", "Tailwind Css"],
-      thumbnail:
-        "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png",
+        "A React.js application for displaying and managing news articles, styled with Tailwind CSS. Provides a user-friendly interface for browsing and reading the latest news updates. Ideal for staying informed and up-to-date with current events.",
+      technologies: ["React.js", "Tailwind CSS"],
+      thumbnail: news,
       liveLink: "https://news-feeder-react-xi.vercel.app/",
     },
     {
       title: "FaceHook",
       description:
-        "A React component for displaying and managing blog comments, including author information and profile images fetched from a server.",
-      technologies: ["Vanilla JS", "Tailwind Css"],
-      thumbnail:
-        "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png",
+        "A React.js application mimicking a social media platform, styled with Tailwind CSS. Features a login page and a clean interface for managing and interacting with user profiles and posts. Ideal for social media enthusiasts looking to explore and simulate social interactions.",
+      technologies: ["React.js", "Tailwind CSS", "CRUD"],
+      thumbnail: fakebook,
       liveLink: "https://facehook-one.vercel.app/login",
     },
     {
       title: "react Blogifi",
       description:
-        "A React component for displaying and managing blog comments, including author information and profile images fetched from a server.",
-      technologies: ["Vanilla JS", "Tailwind Css"],
-      thumbnail:
-        "https://media.dev.to/cdn-cgi/image/width=1600,height=900,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fwhh1lpihw7h587pb2iuc.png",
+        "A React.js application for creating and managing blog posts, styled with Tailwind CSS. Features an intuitive interface for writing, editing, and viewing blog entries. Perfect for bloggers looking to manage and showcase their content with a modern design.",
+      technologies: ["React.js", "Tailwind CSS", "CRUD"],
+      thumbnail: blogify,
       liveLink: "https://react-blogify-tawny-kappa.vercel.app/",
     },
   ],
