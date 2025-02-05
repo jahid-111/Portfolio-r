39fa3f5d2f1f67e471b61356cb730c0e95b39e9f
jahid-111 <jahidjob5@outlook.com>
2025-02-05 14:39:53 +0300

    search_&_activeRoutes : Modify

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,53 +1,58 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const SearchInput = () => {
+  const [search, setSearch] = useState("");
+
+  function handleSearchFrom(e) {
+    e.preventDefault();
+    setSearch(e.target.value);
+  }
+
+  console.log(search);
   return (
     <div className="my-14 relative w-full md:w-6/12">
+      {/* Input Field */}
       <input
-        className="h-10 w-full bg-slate-700 text-gray-300 focus:border-yellow-600 rounded-md pl-4 pr-10"
+        className="h-10 w-full bg-[#464232] focus:border-white focus:outline-dotted rounded-md pl-4 pr-10 placeholder:text-white"
         type="text"
-        placeholder="Search projects"
+        placeholder="Search projects..."
+        onChange={(e) => handleSearchFrom(e)}
       />
 
       <div className="absolute right-3 top-1/2 transform -translate-y-1/2 pointer-events-none">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          version="1.1"
-          width="16"
-          height="16"
-          viewBox="0 0 256 256"
-          fill="currentColor"
+          width="24"
+          height="24"
+          viewBox="0 0 16 16"
         >
-          <g
-            style={{
-              stroke: "none",
-              strokeWidth: 0,
-              strokeDasharray: "none",
-              strokeLinecap: "butt",
-              strokeLinejoin: "miter",
-              strokeMiterlimit: 10,
-              fill: "none",
-              fillRule: "nonzero",
-              opacity: 1,
-            }}
-            transform="translate(1.4 1.4) scale(2.81 2.81)"
-          >
-            <path
-              d="M 88.271 79.927 L 71.011 62.666 c -0.631 -0.631 -1.574 -0.743 -2.327 -0.355 l -2.509 -2.509 c 11.408 -14.576 10.241 -35.733 -2.986 -48.96 C 56.197 3.851 46.902 0 37.015 0 S 17.833 3.851 10.842 10.842 C 3.851 17.833 0 27.128 0 37.015 s 3.851 19.182 10.842 26.173 c 7.171 7.172 16.671 10.798 26.191 10.798 c 8.04 0 16.095 -2.588 22.769 -7.812 l 2.509 2.509 c -0.388 0.753 -0.276 1.696 0.355 2.327 l 17.261 17.261 C 81.041 89.386 82.523 90 84.099 90 c 1.576 0 3.059 -0.614 4.173 -1.729 S 90 85.675 90 84.099 C 90 82.523 89.386 81.041 88.271 79.927 z M 58.596 58.596 c -5.95 5.95 -13.765 8.925 -21.581 8.925 c -7.815 0 -15.631 -2.975 -21.581 -8.925 c -11.9 -11.899 -11.9 -31.262 0 -43.162 c 11.9 -11.9 31.262 -11.899 43.162 0 C 70.496 27.333 70.496 46.696 58.596 58.596 z"
-              style={{
-                stroke: "none",
-                strokeWidth: 1,
-                strokeDasharray: "none",
-                strokeLinecap: "butt",
-                strokeLinejoin: "miter",
-                strokeMiterlimit: 10,
-                fill: "currentColor",
-                fillRule: "nonzero",
-                opacity: 1,
-              }}
-              transform="matrix(1 0 0 1 0 0)"
-            />
-          </g>
+          <path
+            fill="#f7b100"
+            d="m15.7 14.3l-4.2-4.2c-.2-.2-.5-.3-.8-.3c.8-1 1.3-2.4 1.3-3.8c0-3.3-2.7-6-6-6S0 2.7 0 6s2.7 6 6 6c1.4 0 2.8-.5 3.8-1.4c0 .3 0 .6.3.8l4.2 4.2c.2.2.5.3.7.3s.5-.1.7-.3c.4-.3.4-.9 0-1.3M6 10.5c-2.5 0-4.5-2-4.5-4.5s2-4.5 4.5-4.5s4.5 2 4.5 4.5s-2 4.5-4.5 4.5"
+          />
         </svg>
       </div>
+
+      {/* Conditional Bottom Component */}
+      {search && (
+        <div className="absolute bg-[#464232] z-50 my-2 rounded-sm w-full">
+          <div className=" flex flex-col gap-1 justify-center items-start w-full">
+            {[...Array(3)].map((_, index) => (
+              <Link
+                key={index}
+                className="border-b text-white border-gray-500 w-full p-3 rounded-sm hover:bg-[#39372d]"
+              >
+                <p>Project - {index + 1}</p>{" "}
+              </Link>
+            ))}
+            <p className=" text-red-400">
+              {" "}
+              🥲 This Search is Not Implement Yet
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/header/Navigation.jsx
+++ src/components/header/Navigation.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { HiMiniXMark } from "react-icons/hi2";
 import { IoReorderThree } from "react-icons/io5";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 import Brand from "../../assets/svg/brand.svg";
 
 const Navigation = () => {
   const [openMenu, setOpenMenu] = useState(false);
   const routesName = ["Home", "Projects", "Blogs", "Contacts"];
+  const location = useLocation(); // To get the current location/pathname
 
   return (
     <div className="relative">
@@ -16,10 +17,10 @@ const Navigation = () => {
         <div className="fixed inset-0 bg-black/50 backdrop-blur-sm z-40" />
       )}
 
-      <div className="fixed top-0 left-0 w-full h-16  bg-[#11110e] shadow-md shadow-[#232526] px-2 md:px-24 xl:px-32 z-50">
+      <div className="fixed top-0 left-0 w-full h-16 bg-[#11110e] shadow-md shadow-[#232526] px-2 md:px-24 xl:px-32 z-50">
         <div className="h-full flex justify-between items-center">
-          <Link to={"/"} className=" flex justify-center items-center gap-2">
-            <img height={50} width={50} src={Brand} alt="" />
+          <Link to={"/"} className="flex justify-center items-center gap-2">
+            <img height={50} width={50} src={Brand} alt="Brand Logo" />
             <h6 className="text-[#d4a82f] text-xl font-bold">0-𝓭𝓮𝓿</h6>
           </Link>
 
@@ -38,28 +39,33 @@ const Navigation = () => {
 
           <nav
             id="navbar"
-            className={`fixed top-16 md:top-0 right-0 w-3/5 h-screen bg-[#11110e]  flex flex-col items-center gap-3 p-4 transform transition-transform duration-300 ease-in-out ${
+            className={`fixed top-16 md:top-0 right-0 w-3/5 h-screen bg-[#11110e] flex flex-col items-center gap-3 p-4 transform transition-transform duration-300 ease-in-out ${
               openMenu
                 ? "translate-x-0 opacity-100 z-30"
                 : "translate-x-full opacity-0 -z-10"
             } md:relative md:flex md:flex-row md:items-center md:gap-2 md:w-auto md:h-auto md:bg-transparent md:opacity-100 md:translate-x-0 md:z-auto`}
           >
-            {routesName.map((name) => (
-              <NavLink
-                key={name}
-                onClick={() => setOpenMenu(false)}
-                className={({ isActive }) =>
-                  `px-4 py-2 w-full rounded-md text-white text-start transition-colors duration-300 ${
-                    isActive
-                      ? "text-yellow-600 bg-[#3731]"
-                      : "hover:text-yellow-600 hover:bg-[#3731]"
-                  }`
-                }
-                to={`/${name}`}
-              >
-                {name}
-              </NavLink>
-            ))}
+            {routesName.map((name) => {
+              // Check if the current route is the home route
+              const isHomeRoute = location.pathname === "/";
+
+              return (
+                <NavLink
+                  key={name}
+                  onClick={() => setOpenMenu(false)} // Close the menu when a route is clicked
+                  className={({ isActive }) =>
+                    `px-4 py-2 w-full rounded-md text-white text-start transition-colors duration-300 ${
+                      isActive || (isHomeRoute && name === "Home") // Check if current route is active or Home route
+                        ? "text-yellow-600 bg-[#3731]" // Styles for active or Home route
+                        : "hover:text-yellow-600 hover:bg-[#3731]" // Inactive route hover styles
+                    }`
+                  }
+                  to={`/${name === "Home" ? "" : name}`} // Home routes to '/'
+                >
+                  {name === "Home" ? "Home" : name}
+                </NavLink>
+              );
+            })}
           </nav>
         </div>
       </div>
